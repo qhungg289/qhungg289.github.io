05a13e681ca4a4e17ecd09604e2c62ecee614275
Hung Duong Quang <qhungg289@protonmail.com>
2023-03-22 20:28:32 +0700

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  site: 'https://qhungg289.github.io'
+	integrations: [tailwind()],
+	site: "https://qhungg289.github.io",
 });
