82dbbf6d6039704d480c0fa282e0209581f99522
Hung Duong Quang <qhungg289@protonmail.com>
2023-08-31 09:24:36 +0700

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://qhungg289.is-a.dev/",
+    integrations: [tailwind()],
+    site: "https://qhungg289.is-a.dev/"
 });
